461e5126df85fd97aed162be78239365cefdd89d
Halil Durmus <durmushalil@proton.me>
2024-02-11 17:43:19 +0300

    Refactor golden tests

--- tool/generator/test/goldens_test.dart
+++ tool/generator/test/goldens_test.dart
@@ -18,71 +18,16 @@ void main() {
   });
 
   group('Golden testing', () {
-    group('function', () {
-      test('EnumDisplayMonitors', () {
-        const type = 'Windows.Win32.Graphics.Gdi.Apis';
-        final typeDef = MetadataStore.getMetadataForType(type);
-        expect(typeDef, isNotNull);
-        final method = typeDef!.findMethod('EnumDisplayMonitors');
-        expect(method, isNotNull);
-        final projection = FunctionProjection(method!);
-        compareGolden('enumdisplaymonitors.g', projection.toString().format());
-      });
+    testComInterfaceGolden('Windows.Win32.UI.Shell.IFileOpenDialog');
+    testComInterfaceGolden(
+        'Windows.Win32.Networking.NetworkListManager.INetwork');
 
-      test('NtQueryObject', () {
-        const type = 'Windows.Wdk.Foundation.Apis';
-        final typeDef = MetadataStore.getMetadataForType(type);
-        expect(typeDef, isNotNull);
-        final method = typeDef!.findMethod('NtQueryObject');
-        expect(method, isNotNull);
-        final projection = FunctionProjection(method!);
-        compareGolden('ntqueryobject.g', projection.toString().format());
-      });
-    });
+    testFunctionGolden(
+        'Windows.Win32.Graphics.Gdi.Apis', 'EnumDisplayMonitors');
+    testFunctionGolden('Windows.Wdk.Foundation.Apis', 'NtQueryObject');
 
-    group('struct', () {
-      test('DEVMODEW', () {
-        const type = 'Windows.Win32.Graphics.Gdi.DEVMODEW';
-        final typeDef = MetadataStore.getMetadataForType(type);
-        expect(typeDef, isNotNull);
-        final structsToGenerate = loadMap('win32_structs.json');
-        final projection =
-            StructProjection(typeDef!, comment: structsToGenerate[type] ?? '');
-        compareGolden('devmode.g', projection.toString().format());
-      });
-
-      test('VARIANT', () async {
-        const type = 'Windows.Win32.System.Variant.VARIANT';
-        final typeDef = MetadataStore.getMetadataForType(type);
-        expect(typeDef, isNotNull);
-        final structsToGenerate = loadMap('win32_structs.json');
-        final projection =
-            StructProjection(typeDef!, comment: structsToGenerate[type] ?? '');
-        compareGolden('variant.g', projection.toString().format());
-      });
-    });
-
-    group('COM interface', () {
-      test('IFileOpenDialog', () {
-        const type = 'Windows.Win32.UI.Shell.IFileOpenDialog';
-        final typeDef = MetadataStore.getMetadataForType(type);
-        expect(typeDef, isNotNull);
-        final comTypesToGenerate = loadMap('com_types.json');
-        final projection = ComInterfaceProjection(typeDef!,
-            comment: comTypesToGenerate[type] ?? '');
-        compareGolden('ifileopendialog.g', projection.toString().format());
-      });
-
-      test('INetwork', () async {
-        const type = 'Windows.Win32.Networking.NetworkListManager.INetwork';
-        final typeDef = MetadataStore.getMetadataForType(type);
-        expect(typeDef, isNotNull);
-        final comTypesToGenerate = loadMap('com_types.json');
-        final projection = ComInterfaceProjection(typeDef!,
-            comment: comTypesToGenerate[type] ?? '');
-        compareGolden('inetwork.g', projection.toString().format());
-      });
-    });
+    testStructGolden('Windows.Win32.Graphics.Gdi.DEVMODEW');
+    testStructGolden('Windows.Win32.System.Variant.VARIANT');
   });
 
   tearDownAll(MetadataStore.close);
@@ -94,6 +39,58 @@ void compareGolden(String fileName, String content) {
   File('test/goldens/$fileName.comparison').writeAsStringSync(content);
   final golden = File('test/goldens/$fileName.golden').readAsStringSync();
   expect(content, equals(golden.convertLineEndingsToLF()));
+}
+
+void testComInterfaceGolden(String interfaceName) {
+  test(interfaceName, () {
+    final typeDef = MetadataStore.getMetadataForType(interfaceName);
+    expect(
+      typeDef,
+      isNotNull,
+      reason: '`$interfaceName` type is not found in the metadata.',
+    );
+    final comTypesToGenerate = loadMap('com_types.json');
+    final projection = ComInterfaceProjection(typeDef!,
+        comment: comTypesToGenerate[interfaceName] ?? '');
+    final fileName = typeDef.safeIdentifier.toLowerCase();
+    compareGolden('interfaces/$fileName.g', projection.toString().format());
+  });
+}
+
+void testFunctionGolden(String parent, String functionName) {
+  test('function $parent.$functionName', () {
+    final typeDef = MetadataStore.getMetadataForType(parent);
+    expect(
+      typeDef,
+      isNotNull,
+      reason: '`$parent` type is not found in the metadata.',
+    );
+    final method = typeDef!.findMethod(functionName);
+    expect(
+      method,
+      isNotNull,
+      reason: '`$functionName` function is not found in `$parent`.',
+    );
+    final projection = FunctionProjection(method!);
+    final fileName = method.nameWithoutEncoding.safeIdentifier.toLowerCase();
+    compareGolden('functions/$fileName.g', projection.toString().format());
+  });
+}
+
+void testStructGolden(String type) {
+  test('struct $type', () {
+    final typeDef = MetadataStore.getMetadataForType(type);
+    expect(
+      typeDef,
+      isNotNull,
+      reason: '`$type` type is not found in the metadata.',
+    );
+    final structsToGenerate = loadMap('win32_structs.json');
+    final projection =
+        StructProjection(typeDef!, comment: structsToGenerate[type] ?? '');
+    final fileName = typeDef.safeIdentifier.toLowerCase();
+    compareGolden('structs/$fileName.g', projection.toString().format());
+  });
 }
 
 extension on String {
